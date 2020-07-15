2fc18e51107d4e06b911aa9e3df5e283faf2b162
shravanc <shravan007.c@gmail.com>
2020-07-15 23:59:45 +0100

    addede sing out call

--- web_app/test/controllers/sessions_controller_test.rb
+++ web_app/test/controllers/sessions_controller_test.rb
@@ -2,10 +2,16 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
 
-test "Should delete session" do
+test "test Login"do
   user = user = User.create(username: 'sample', email: 'sample@gmail.com', password: 'password', verified: true)
   post '/sessions', params: {'session': {'username': user.username, 'password': 'password'}}
-  assert_response 201
+  assert_response 200
+end
+
+test "Sign out" do
+  session = sessions(:user_1_session)
+  delete "/sessions/#{session.session_id}"
+  assert_response 302
 end
 
 end
