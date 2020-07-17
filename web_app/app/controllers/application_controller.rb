593eac7cea8223dfd3cb5be0ede58bae47a0598b
shravanc <shravan007.c@gmail.com>
2020-07-17 12:01:24 +0100

    changes in view

--- web_app/app/controllers/application_controller.rb
+++ web_app/app/controllers/application_controller.rb
@@ -2,19 +2,25 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
 
   def validate_username
-    username = params[:action] == 'forgot_password' ? params[:user][:username] : params[:session][:username]
-    logger.warn username
-    user = User.find_by_username(username)
+    user = User.find_by_username(username['username'])
     if user.nil?
       flash[:invalid_credentials] = 'Invalid username'
       redirect_to action: 'new'
+      return
     elsif !user.verified
       flash[:invalid_credentials] = 'Account not verified'
       redirect_to action: 'new'
+      return
     elsif user.failure_count >= 3
       flash[:invalid_credentials] = 'Maximum Attempt Reached!'
       redirect_to action: 'new'
+      return
     end
     @current_user = user
+  end
+
+  private
+  def username
+    params.require(:session).permit(%i[username])
   end
 end
--- web_app/app/controllers/sessions_controller.rb
+++ web_app/app/controllers/sessions_controller.rb
@@ -1,6 +1,11 @@
 class SessionsController < ApplicationController
   before_action :validate_username, only: :create
 
+  # 
+  def index
+    redirect_to action: 'new'
+  end
+
   # Form for User Login
   def new; end
 
@@ -8,17 +13,18 @@ class SessionsController < ApplicationController
   def create
     status, data = @current_user.validate_credentials(credentials)
     if status
-      session = Session.new(session_attributes)
-      if session.save
+      session = Session.create!(session_attributes)
+      if session.valid?
         @session_id = session.session_id
         render :create, status: :created
       else
         flash[:invalid_credentials] = 'Please try again!'
+        redirect_to action: 'new'
       end
     else
       flash[:invalid_credentials] = data[:message]
+      redirect_to action: 'new'
     end
-    redirect_to action: 'new'
   end
 
   # Log out API request, to delete session for a User
